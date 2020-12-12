32c3bdd1fa960232676873e2644bd0a8e72e0634
guysh2020 <61948800+guysh2020@users.noreply.github.com>
2020-12-12 11:11:27 +0200

    initial

--- src/model/IModel.java
+++ src/model/IModel.java
@@ -1,4 +1,16 @@
 package model;
 
 public interface IModel {
+
+    public void AddCost() throws CostMannagerException;
+    public void DeleteCost() throws CostMannagerException;
+    public void AddCategory() throws CostMannagerException;
+
+    public void GeneratePieSummary() throws CostMannagerException;
+    public void GenerateRegularSummary() throws CostMannagerException;
+
+    public void SortItems() throws CostMannagerException;
+
+//    public void fromDB() throws CostMannagerException;
+//    public void toDB() throws CostMannagerException;
 }
